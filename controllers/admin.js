cd0b8d40a8fa88b1db7402544de9da7da22e47df
ahnitin <nitinahuja240@gmail.com>
2023-10-19 11:38:26 +0530

    Design the User and Product relationship Ass-20

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
+    description: description,
+
   })
+  //Product.create()
     .then(result => {
       // console.log(result);
       console.log('Created Product');
@@ -34,9 +36,12 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect('/');
   }
+
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(product => {
+  req.user.getProducts({where:{id:prodId}})
+  //Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect('/');
       }
@@ -56,7 +61,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById(prodId)
+  Product.findByPk(prodId)
     .then(product => {
       product.title = updatedTitle;
       product.price = updatedPrice;
@@ -72,7 +77,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  user.req
+  .getProducts()
+  //Product.findAll()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -85,7 +92,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId)
+  Product.findByPk(prodId)
     .then(product => {
       return product.destroy();
     })
--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('app-product', 'root', 'good@123', {
   dialect: 'mysql',
   host: 'localhost'
 });
